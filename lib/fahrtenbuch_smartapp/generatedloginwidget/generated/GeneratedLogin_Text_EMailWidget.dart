67cef49c008e800f99fd2f4c469b270eda8471ff
Samusin95 <alexandergross95@gmx.at>
2021-10-06 14:33:47 +0200

    Erster Stand

--- lib/fahrtenbuch_smartapp/generatedloginwidget/generated/GeneratedLogin_Text_EMailWidget.dart
+++ lib/fahrtenbuch_smartapp/generatedloginwidget/generated/GeneratedLogin_Text_EMailWidget.dart
@@ -18,8 +18,13 @@ class GeneratedLogin_Text_EMailWidget extends StatelessWidget {
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(5.429718971252441),
-        child: Container(
-          color: Color.fromARGB(255, 251, 251, 251),
+        child: TextField(
+
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            color: Color.fromARGB(255, 251, 251, 251),
+          ),
+
         ),
       ),
     );
