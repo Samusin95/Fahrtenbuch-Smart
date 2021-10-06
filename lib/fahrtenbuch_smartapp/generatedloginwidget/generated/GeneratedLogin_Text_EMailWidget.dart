050bf45dc8e324b80ac932c487e4d0439601c544
Julia-jmb <julia.m.beck@gmx.at>
2021-10-06 14:45:22 +0200

    weiße Textfeld

--- lib/fahrtenbuch_smartapp/generatedloginwidget/generated/GeneratedLogin_Text_EMailWidget.dart
+++ lib/fahrtenbuch_smartapp/generatedloginwidget/generated/GeneratedLogin_Text_EMailWidget.dart
@@ -19,10 +19,10 @@ class GeneratedLogin_Text_EMailWidget extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(5.429718971252441),
         child: TextField(
-
-          decoration: InputDecoration(
+           decoration: InputDecoration(
             border: OutlineInputBorder(),
-            color: Color.fromARGB(255, 251, 251, 251),
+            filled: true,
+            fillColor: Colors.white,
           ),
 
         ),
