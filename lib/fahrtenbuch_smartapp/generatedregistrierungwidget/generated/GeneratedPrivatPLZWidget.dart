068b9ff49829b083d7dd3fc0b8a8c4eeba84ffc4
Alexander.GROSS <alexandergross95@gmx.at>
2021-10-25 10:59:24 +0200

    neuer Stand (Privat PLZ -> in Datenbank)

--- lib/fahrtenbuch_smartapp/generatedregistrierungwidget/generated/GeneratedPrivatPLZWidget.dart
+++ lib/fahrtenbuch_smartapp/generatedregistrierungwidget/generated/GeneratedPrivatPLZWidget.dart
@@ -23,7 +23,7 @@ class GeneratedPrivatPLZWidget extends StatelessWidget {
               bottom: null,
               width: 82.2244873046875,
               height: 26.0,
-              child: GeneratedRegistrierung_Text_PrivatPLZWidget(),
+              child: GeneratedRegistrierung_Text_PrivatPLZWidget(key: key),
             ),
             Positioned(
               left: 0.0,
